e0c9a2a8b00dd14cce41496603e8368342009a4b
Grzegorz.Bizuga <grzegorz.bizuga@gmail.com>
2024-03-01 14:42:21 +0100

    dodanie klasy Actor

---
+++ movie_library/src/main/java/org/example/models/Actor.java
@@ -0,0 +1,13 @@
+package org.example.models;
+
+public class Actor {
+    private String firstName;
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+}
